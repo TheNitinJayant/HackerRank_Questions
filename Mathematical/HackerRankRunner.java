8cefb7d6e1dceca9c00dcfc850fb03ffe449153b
Nitin Jayant <nitin.jayant@Nitin-kumar-Singh-MBP.local>
2021-10-19 20:08:27 +0530

    first

---
+++ Mathematical/HackerRankRunner.java
@@ -0,0 +1,9 @@
+package HackerRank_Questions.Mathematical;
+
+public class HackerRankRunner {
+
+    public void run(){
+        MaximumDraws maximumDraws = new MaximumDraws();
+        maximumDraws.mainMaximumDraws();
+    }
+}
---
+++ Mathematical/MaximumDraws.java
@@ -0,0 +1,12 @@
+package HackerRank_Questions.Mathematical;
+
+public class MaximumDraws {
+
+    public void mainMaximumDraws(){
+        System.out.println(maximumDraws(3));
+    }
+
+    public int maximumDraws(int noOfColors){
+        return noOfColors+1;
+    }
+}
